4d05d6f750f45fc23e25434e0ec56233553f07de
Jamie Robinson <jamiegribblerobinson@gmail.com>
2025-04-16 23:31:02 +1000

    Enemy spawner tool done!

--- IGB100Game/Assets/Scripts/Game/EnemySpawner.cs
+++ IGB100Game/Assets/Scripts/Game/EnemySpawner.cs
@@ -3,69 +3,101 @@ using UnityEngine;
 
 public class Wave
 {
-    public string waveName;
+    [Tooltip("Name of the wave... pretty obvious..")]
+    [SerializeField] string waveName;
+
+    [Tooltip("How many enemies spawn per spawn cycle")]
     public int enemyDensity;
-    // Types of enemies spawned in wave
-    // wave frequency
+
+    [Tooltip("How fast each spawn cycle is (seconds)")]
+    public float spawnFrequency;
+
+    [Tooltip("Types of enemies that will be randomly spawned this wave")]
+    public GameObject[] enemyType;
 }
-// Starting on wave system
 
 
 public class EnemySpawner : MonoBehaviour
 {
-    [SerializeField] float spawnFrequency;
-    private float spawnTimer;
+    [Tooltip("Distance enemies will spawn from the centre of the screen e.g. 1 = 1 screen size")]
+    [SerializeField] float spawnDistance = 1f;
 
+    [Tooltip("How fast each wave will be (seconds)")]
+    [SerializeField] float waveFrequency;
+
+    [Tooltip("How many enemies can be in the game world at once")]
+    [SerializeField] int maxEnemyCapacity;
+
+    [Tooltip("Array of all waves")]
     [SerializeField] Wave[] waves;
-    [SerializeField] GameObject tempEnemy;
-    // Max Enemy Spawn
+
+    private GameObject[] enemiesSpawned;
+
+    private float spawnTimer;
+    private float waveTimer;
+
+    private Wave currentWave;
+    private int currentWaveNumber = 0;
 
     void Update()
     {
+        currentWave = waves[currentWaveNumber];
+
+        enemiesSpawned = GameObject.FindGameObjectsWithTag("Enemy");
+
+        waveTimer += Time.deltaTime;
         spawnTimer += Time.deltaTime;
         
-        if (spawnTimer >= spawnFrequency)
+        if (spawnTimer >= currentWave.spawnFrequency && enemiesSpawned.Length < maxEnemyCapacity)
         {
             spawnTimer = 0.0f;
-            EnemySpawnPoint();
+            for (int i = 0; i < currentWave.enemyDensity; i++)
+            {
+                SpawnEnemy();
+            }
+        }
 
+        if (waveTimer >= waveFrequency)
+        {
+            currentWaveNumber++;
+            waveTimer = 0;
         }
     }
 
-    private void EnemySpawnPoint()
+    void SpawnEnemy()
     {
         Vector2 randomPosition;
-
+        GameObject randomEnemy = currentWave.enemyType[Random.Range(0, currentWave.enemyType.Length)];
         
-        if (Random.Range(0.0f, 1.0f) > 0.5f) // Spawn on the sides of the screen
+        if (Random.Range(0f, 1f) > 0.5f) // Spawn on the sides of the screen
         {
-            randomPosition.y = Random.Range(-1.0f, 2.0f);
-
-            if (Random.Range(0.0f, 1.0f) > 0.5f) // Spawn right
+            randomPosition.y = Random.Range(0.5f - spawnDistance, 0.5f + spawnDistance);
+            
+            if (Random.Range(0f, 1f) > 0.5f) // Spawn right
             {
-                randomPosition.x = 2.0f;
+                randomPosition.x = 0.5f + spawnDistance;
             }
             else // Spawn left
             {
-                randomPosition.x = -1.0f;
+                randomPosition.x = 0.5f - spawnDistance;
             }
         }
         else // Spawn on top/bottom of the screen
         {
-            randomPosition.x = Random.Range(-1.0f, 2.0f);
+            randomPosition.x = Random.Range(0.5f - spawnDistance, 0.5f + spawnDistance);
 
-            if (Random.Range(0.0f, 1.0f) > 0.5f) // Spawn top
+            if (Random.Range(0f, 1f) > 0.5f) // Spawn top
             {
-                randomPosition.y = 2.0f;
+                randomPosition.y = 0.5f + spawnDistance;
             }
             else // Spawn bottom
             {
-                randomPosition.y = -1.0f;
+                randomPosition.y = 0.5f - spawnDistance;
             }
         }
 
         Vector2 spawnPos = Camera.main.ViewportToWorldPoint(randomPosition);
-        Instantiate(tempEnemy, spawnPos, Quaternion.identity);
+        Instantiate(randomEnemy, spawnPos, Quaternion.identity);
     }
 
 }
