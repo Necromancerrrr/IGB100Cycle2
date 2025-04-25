20ab865e703906d966d8df5f93d49b4c28afe40c
Olivia <83168370+OliviaGoBrrr@users.noreply.github.com>
2025-04-25 15:44:51 +1000

    Small UI Updates

--- IGB100Game/Assets/Scripts/Player/InventoryManager.cs
+++ IGB100Game/Assets/Scripts/Player/InventoryManager.cs
@@ -171,6 +171,10 @@ public class InventoryManager : MonoBehaviour
             {
                 return;
             }
+            else if (weaponSlots[5] != null && passiveItemSlots[5] != null)
+            {
+                return;
+            }
 
             int upgradeType;
 
