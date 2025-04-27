7a5148e55a105c699d58ed026521d9ffb507069c
ThePinkOtter <SomePinkOtter@gmail.com>
2025-04-28 00:40:15 +1000

    Help Menu (Scuffed)

---
+++ IGB100Game/Assets/Scripts/MainMenu/HelpOverlay.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class HelpOverlay : MonoBehaviour
+{
+    private bool Enabled = false;
+    public void ButtonPress()
+    {
+        Enabled = true;
+        gameObject.SetActive(Enabled); 
+    }
+    private void Update()
+    {
+        if (Enabled = true && Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1))
+        {
+            Enabled = false;
+            gameObject.SetActive(Enabled);
+        }
+    }
+}
--- IGB100Game/Assets/Scripts/MainMenu/SceneController.cs
+++ IGB100Game/Assets/Scripts/MainMenu/SceneController.cs
@@ -4,12 +4,16 @@ using UnityEngine.SceneManagement;
 
 public class SceneController : MonoBehaviour
 {
+    [SerializeField] private HelpOverlay helpMenu;
     public void SceneChange(string name)
     {
         Time.timeScale = 1.0f;
         SceneManager.LoadScene(name);
     }
-
+    public void Help()
+    {
+        helpMenu.ButtonPress();
+    }
     public void Quit()
     {
         Application.Quit();
