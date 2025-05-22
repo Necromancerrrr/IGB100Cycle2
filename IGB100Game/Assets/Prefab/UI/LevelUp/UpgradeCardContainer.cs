7c5677bfdd07a39e32e3899200c7f8d1d971a323
Jamie Robinson <jamiegribblerobinson@gmail.com>
2025-05-22 22:46:04 +1000

    Tweening done for tonight

--- IGB100Game/Assets/Prefab/UI/LevelUp/UpgradeCardContainer.cs
+++ IGB100Game/Assets/Prefab/UI/LevelUp/UpgradeCardContainer.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using DG.Tweening;
+using Unity.VisualScripting;
 
 public class UpgradeCardContainer : MonoBehaviour
 {
@@ -10,55 +11,79 @@ public class UpgradeCardContainer : MonoBehaviour
 
     [SerializeField] private Transform[] cards;
 
-    private float duration = 1f;
+    private float duration = 0.5f;
     private float timer;
     private bool currentlyTweening = false;
     private int currentCardNum = 0;
 
     private GameManager gameManager;
 
+    //Sequence initialSequence = DOTween.Sequence();
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        //initialSequence.Append(cards[0].DOScale(0f, duration).SetUpdate(true));
+
         //initialScale = transform.localScale;
         //transform.localScale = new Vector3(0, 0, 0);
 
         gameManager = FindFirstObjectByType<GameManager>();
+        for (int i = 0; i < cards.Length; i++)
+        {
+            cards[i].localScale = new Vector3(0, 0, 0);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        //timer += Time.deltaTime;
-        /*
-        if (gameObject.activeSelf && currentlyTweening == false)
+        if (gameManager.currentState != GameManager.GameState.LevelUp)
         {
-            test(firstCard);
+            for (int i = 0; i < cards.Length; i++)
+            {
+                cards[i].localScale = new Vector3(0, 0, 0);
+            }
         }
-        */
 
         if (gameManager.currentState == GameManager.GameState.LevelUp && currentlyTweening == false)
         {
-            InitialTween(0);
+            InitialTween(currentCardNum);
+            Debug.Log("AAAAA");
         }
-        //Debug.Log(firstCard.rotation);
+
+        if (cards[currentCardNum].localScale.x > 0.5)
+        {
+            Debug.Log("AAAA");
+            currentCardNum++;
+            currentlyTweening = false;
+        }
     }
 
-
-
     
+
+    //TweenCallback
+    //.OnComplete(InitialTween(currentCard))
+
     public TweenCallback InitialTween(int currentCard)
     {
-        if (currentCard == cards.Length + 1) return null;
-
+        if (currentCard == cards.Length) return null;
+        Debug.Log(currentCard);
         Transform currentCardTransform = cards[currentCard];
 
         currentlyTweening = true;
-        currentCardTransform.DOScale(0f, duration).SetUpdate(true);
-        currentCardTransform.DOLocalRotate(new Vector3(0, 0, 360), duration, RotateMode.FastBeyond360).SetUpdate(true).OnComplete(InitialTween(currentCard+1));
-        
+
+        currentCardTransform.DOScale(1f, duration).SetUpdate(true).SetEase(Ease.OutQuint);
+        currentCardTransform.DOLocalRotate(new Vector3(0, 0, -360), duration, RotateMode.FastBeyond360).SetUpdate(true).SetEase(Ease.OutQuint).OnComplete(TweenCompleted);
 
         return null;
+    }
+
+    public void TweenCompleted()
+    {
+        Debug.Log("AAAA");
+        //currentCardNum++;
+        //currentlyTweening = false;
     }
 
     /*
